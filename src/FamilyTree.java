12f123e23cecb70592b97068572fb845fff6e1db
amccann1 <amccann1@10.120.22.45>
2016-11-22 10:31:17 -0500

    Doing This Right This Time
    Adding the sparse to the src folder

---
+++ src/FamilyTree.java
@@ -0,0 +1,24 @@
+
+
+public class FamilyTree {
+	
+	public FamilyTree()
+	{
+		
+	}
+	
+	public void addMember(String name, Integer birthDate, String placeOfBirth)
+	{
+		
+	}
+	
+	public void removeMember(String Name, Integer birthDate, String placeOfBirth)
+	{
+		
+	}
+	
+	public void toString(String Name, Integer birthDate, String placeOfBirth)
+	{
+		
+	}
+}
